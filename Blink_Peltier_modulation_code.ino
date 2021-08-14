d7c87e2b4299aece9cfa381dcf9e014bd5c4edd6
Mattboss10 <86135050+Mattboss10@users.noreply.github.com>
2021-08-14 17:05:37 -0500

    Add files via upload

---
+++ Blink_Peltier_modulation_code.ino
@@ -0,0 +1,37 @@
+/*
+  Blink
+
+  Turns an LED/any device(pelteir in my case) on for 5 seconds, then off for 3 second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 28 May 2021
+  by Matthew Cohen
+  modified 28 May 2021
+  by Matthew Cohen
+  modified 28 May 2021
+  by Matthew Cohen
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever and ever and ever to infinity 
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(2000);                       // wait for a second
+}
