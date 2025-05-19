8d559a92a66e1cdec46afb048a06880118cc54c3
Matthew A Cohen <86135050+Mattboss10@users.noreply.github.com>
2025-05-18 22:08:46 -0500

    Update and rename Blink_Peltier_modulation_code.ino to PWM Peltier modulation code.ino
    -Update:May 18, 2025, Future more advanced matthew here lol, I have learned about complexity and optimization problems and now realize that this is essentially an NP-Hard problem in the general form.
    -Problem: Find the best frequency and duty cycle that minimizes cold-side temperature, subject to no heatsink. 
    -This problem can be a great intro into embedded control systems and computational complexity in real-world engineering.
    -It can be viewed as a Discrete-time control optimization with a black-box objective function.
      
    -This thermal optimization problem is NP-hard in general form, especially if you treat it as a high-resolution scheduling problem without an analytical model.

--- PWM Peltier modulation code.ino
+++ PWM Peltier modulation code.ino
@@ -23,15 +23,14 @@
 */
 
 // the setup function runs once when you press reset or power the board
+const int peltierPin = 9; // PWM-capable pin sending partial power signal .
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(peltierPin, OUTPUT);
 }
 
-// the loop function runs over and over again forever and ever and ever to infinity 
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);                       // wait for a second
+void loop() { //creating a loop to run endlessely until circuit is switched off
+  analogWrite(peltierPin, 128); // 50% power output to Peltier instead of always ON
+  delay(10000);                 // run for 10 seconds
 }
+
